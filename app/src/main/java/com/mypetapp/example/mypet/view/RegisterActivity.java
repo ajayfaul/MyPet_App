19ec6f79ff999124c1cd1d7a3936ec8ad8dea5e0
ajayfaul <muhammadfajarm@gmail.com>
2019-01-22 00:43:32 +0700

    fix register and screen orientation

--- app/src/main/java/com/mypetapp/example/mypet/view/RegisterActivity.java
+++ app/src/main/java/com/mypetapp/example/mypet/view/RegisterActivity.java
@@ -1,15 +1,33 @@
 package com.mypetapp.example.mypet.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.mypetapp.example.mypet.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnLogin = findViewById(R.id.btn_Login);
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_Login:
+                    Intent moveIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(moveIntent);
+                    break;
+            }
     }
 }
