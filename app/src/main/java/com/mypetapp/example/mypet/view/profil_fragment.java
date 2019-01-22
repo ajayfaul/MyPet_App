a710e688c2a1ecbd495dc19888ea0ac8bf4d75e4
Ajayfaul <muhammadfajarm@gmail.com>
2019-01-22 14:38:57 +0700

    fix button add dan edit done

--- app/src/main/java/com/mypetapp/example/mypet/view/profil_fragment.java
+++ app/src/main/java/com/mypetapp/example/mypet/view/profil_fragment.java
@@ -1,5 +1,6 @@
 package com.mypetapp.example.mypet.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,15 +11,42 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mypetapp.example.mypet.R;
 
-public class profil_fragment extends Fragment {
+public class profil_fragment extends Fragment{
 
-    Button btnadd, btnedit;
+    private Button btnadd, btnedit;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_profil, null);
+    }
+
+    @Override
+    public void onViewCreated(View v, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(v, savedInstanceState);
+
+            btnadd = (Button) v.findViewById(R.id.btndadd);
+            btnadd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent moveIntent = new Intent(getActivity(), adoptionActivity.class);
+                    getActivity().startActivity(moveIntent);
+                }
+            });
+
+            btnedit = (Button) v.findViewById(R.id.btnservice);
+            btnedit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent moveIntent2 = new Intent(getActivity(), adoptionActivity.class);
+                    getActivity().startActivity(moveIntent2);
+                }
+            });
     }
 }
